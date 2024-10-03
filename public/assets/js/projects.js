28950e535ed43866288554fa11310a735f6f1b9a
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-10-03 13:05:06 +0700

    add functionalities

--- public/assets/js/projects.js
+++ public/assets/js/projects.js
@@ -1,29 +1,77 @@
-const spaceId = '65fnxbl95zo8'
-const accessToken = 'tp4_2D-3VEm5POrLw7FjD3DLFxASS3RduMrVjqZ-7wQ'
+const projectsContainer = document.getElementById("projects-container")
+const loadingText = document.getElementById("project-loading")
+
+const createProjectElement = (title, desc, link) => {
+    const projectDiv = document.createElement("div")
+    projectDiv.classList.add("project")
+    
+    const h = document.createElement("h3")
+    h.innerText = title
+    projectDiv.appendChild(h)
+
+    const p = document.createElement("p")
+    p.innerText = desc
+    projectDiv.append(p)
+
+    const a = document.createElement("a")
+    a.innerText = "Check it out!"
+    a.setAttribute("href", link)
+    a.setAttribute("target", "_blank")
+    a.setAttribute("rel", "noopener,noreferrer")
+    projectDiv.appendChild(a)
+
+    return projectDiv
+}
 
 const fetchProjects = async () => {
-    const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/master/entries?access_token=${accessToken}&content_type=projects`;
-
+    const url = `https://minijakka-api.jakka.workers.dev/getprojects`
+    let dataArray = []
     try {
-        const response = await fetch(url);
+        const response = await fetch(url)
         if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
+            throw new Error(`Error: ${response.status}`)
         }
 
-        const data = await response.json();
-        
-        // Logging the fetched project entries
-        console.log(data.items);
-        
-        // Handle and display the project entries
-        data.items.forEach(item => {
-            console.log(`Title: ${item.fields.title}`);
-            console.log(`Description: ${item.fields.description}`);
-            // You can map other fields from the 'Projects' content type here
-        });
-    } catch (error) {
-        console.error('Failed to fetch projects:', error);
-    }
-};
+        const data = await response.json()
+        data['items'].forEach(x => {
+            const { title, description, githubLink, projectLink } = x.fields
+            const { createdAt } = x.sys
+            const link = projectLink || githubLink
+            const data = {
+                title,
+                description,
+                link,
+                date: createdAt,
+            }
+            dataArray.push(data)
+        })
+        dataArray.sort((a, b) => {
+            return Date.parse(b.date) - Date.parse(a.date)
+        })
+        console.log(dataArray)
 
-fetchProjects();
+    } catch (error) {
+        console.error('Failed to fetch projects:', error)
+    }
+    return dataArray
+}
+
+fetchProjects().then(data => {
+    loadingText.remove()
+
+    for (let i = 0; i < data.length; i++){
+        const ea = data[i]
+        const ele = createProjectElement(ea['title'], ea['description'], ea['link'])
+        projectsContainer.appendChild(ele)
+        if (i !== data.length - 1){
+            const hr = document.createElement("hr")
+            hr.style.borderTop = "1px solid #a5a5a5"
+            hr.style.width = "100%"
+            hr.style.marginTop = "1.5rem"
+            hr.style.marginBottom = "1.5rem"
+            hr.style.borderRadius = "1rem"
+            hr.style.opacity = "25%"
+            projectsContainer.append(hr)
+        }
+    }
+})
